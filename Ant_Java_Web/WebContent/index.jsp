268c4f79081e084c5d8e3c08019ba3ca36a701f2
root <root@ip-172-31-42-162.ap-south-1.compute.internal>
2023-07-22 09:19:07 +0000

    third

---
+++ Ant_Java_Web/WebContent/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World and Nevil 2!</h2>
+</body>
+</html>
---
+++ sentry-react/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+Sentry.init({
+  release: "portfolio@0.1.0",
+  autoSessionTracking: false,
+  dsn: "https://9956607a468a4e3db3a01af294206f2d@o1155879.ingest.sentry.io/4505476734976000",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
